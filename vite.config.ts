756438ccab34d5f467a5d16330b390e108959769
Shahid Shabbir <shahidshabbirse@gmail.com>
2024-12-10 12:27:08 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server:{
+port:5173,
+strictPort:false,
+},
+preview:{
+port:4173,
+strictPort:false,
+}
 })
